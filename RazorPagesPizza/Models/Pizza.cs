a4fedb0193647ef0393c210736651c678e6a8e87
Jon Galloway <jongalloway@gmail.com>
2021-08-18 15:12:39 -0700

    Add project files.

---
+++ RazorPagesPizza/Models/Pizza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesPizza.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
---
+++ RazorPagesPizza/Pages/Pizza.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesPizza.Models;
+using RazorPagesPizza.Services;
+using System.Collections.Generic;
+
+namespace RazorPagesPizza.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public List<Pizza> pizzas;
+        public void OnGet()
+        {
+            pizzas = PizzaService.GetAll();
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
+        }
+
+        [BindProperty]
+        public Pizza NewPizza { get; set; }
+
+        public IActionResult OnPost()
+        {
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+
+        public string GlutenFreeText(Pizza pizza)
+        {
+            if (pizza.IsGlutenFree)
+                return "Gluten Free";
+            return "Not Gluten Free";
+        }
+    }
+}
