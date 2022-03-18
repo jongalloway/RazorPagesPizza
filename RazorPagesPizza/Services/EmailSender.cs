b41b4e627a97419b3b2ea6079b128461d330be6f
Cam Soper <cam.soper@microsoft.com>
2022-03-18 16:13:31 -0500

    email sender

--- RazorPagesPizza/Services/EmailSender.cs
+++ RazorPagesPizza/Services/EmailSender.cs
@@ -5,12 +5,14 @@ public class EmailSender : IEmailSender
 
     public Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
+        Console.WriteLine();
         Console.WriteLine("Email Confirmation Message");
         Console.WriteLine("--------------------------");
         Console.WriteLine($"TO: {email}");
         Console.WriteLine($"SUBJECT: {subject}");
         Console.WriteLine($"CONTENTS: {htmlMessage}");
-
+        Console.WriteLine();
+        
         return Task.CompletedTask;
     }
 }
